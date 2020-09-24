a277e1fa7e4f49cc98739abe5d777519116516b4
nag <nag@192.168.1.11>
2020-09-23 20:14:32 -0500

    Lamba Expressions HelloWorld

---
+++ DallasDreams/src/ind/bos/atl/LambdaDemo.java
@@ -0,0 +1,18 @@
+package ind.bos.atl;
+
+interface A {
+	 void show(int i); 
+	
+}
+
+public class LambdaDemo {
+	public static void main(String[] args) {
+
+		A obj;
+		obj = i -> System.out.println("Hello\t" + i);
+		obj.show(9);
+
+	}
+
+}
+
