88017c46fbba1caedd3f4e7ca932b997125b2190
Nag <nagu.java@gmail.com>
2021-07-06 14:31:17 -0500

    just cleaned up the code

--- DallasDreams/src/ind/bos/atl/BubbleSort.java
+++ DallasDreams/src/ind/bos/atl/BubbleSort.java
@@ -1,21 +1,15 @@
 package ind.bos.atl;
 
-import java.util.ArrayList;
 
 public class BubbleSort {
 	
 	public static void main(String[] args) {
-		java.util.List<Integer> numbers = new ArrayList<Integer>();
-		//numbers.add(e)
-		
 		int[] tickets = {8,9,2,5,4,3,7,1};
 		sort(tickets);
 		
 	}
 	
 	public static void sort(int []list) {
-		int ctr=0;
-		int[] result = new int[8];
 		for(int i=0;i<=list.length-1;i++) {
 			for(int j=i;j<=list.length-1;j++) {
 					if(list[i]>list[j]) {
