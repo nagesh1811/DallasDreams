a6ba7b51c9a3493b93e7df4ce2a2842ae8410ca6
Nag <nagu.java@gmail.com>
2021-08-18 22:46:15 -0500

    square of each element in array and sort them.

---
+++ DallasDreams/src/com/bom/arrays/SquaresOfSortedArray.java
@@ -0,0 +1,33 @@
+package com.bom.arrays;
+
+public class SquaresOfSortedArray {
+    public static void main(String[] args) {
+        int[] input = {-4, -1, 0, 3, 10};
+
+        int[] output = FetchSquaresOfSortedArray(input);
+        for(int k:output) {
+            System.out.print(", "+k);
+        }
+    }
+
+    public static int[] FetchSquaresOfSortedArray(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int pointer = nums.length-1;
+        int [] result = new int[nums.length];
+
+        while(left<=right) {
+            if(Math.abs(nums[left])< Math.abs(nums[right])){
+                result[pointer] = nums[right]*nums[right];
+                pointer--;
+                right--;
+            } else {
+                result[pointer] = nums[left]*nums[left];
+                pointer--;
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
